439e37911001eaef6fd85d03da02239f1fef0d0c
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 01:08:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function fibonacci(num) {
-// your code here
+  if (num <= 0) {
+    return "Please enter a positive integer.";
+  }
 
-	if(num == 1 || num == 0){
-		return 0;
-	}
+  if (num === 1) {
+    return [0];
+  }
 
-	if(num == 2){
-		return 1;
-	}
+  if (num === 2) {
+    return [0, 1];
+  }
+
+  let fibSequence = [0, 1];
+
+  for (let i = 2; i < num; i++) {
+    fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
+  }
+
+  return fibSequence;
 }
 
 module.exports = fibonacci;
